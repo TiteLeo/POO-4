b70e40c31573f1ab1e4fbe0e1e8707eeaf17ccc2
ELEONORE GOUZE <90243662+TiteLeo@users.noreply.github.com>
2021-10-28 22:33:22 +0200

    Update car.php

--- car.php
+++ car.php
@@ -6,17 +6,20 @@ class car extends Vehicle
     private bool $hasParkBrake = true;
 }
 
+ public function start()
+	{
+		if ($this->hasParkBrake) {
+			throw new Exception('Your ParkBrake Dude!!');
+		}
+		echo 'Go baby!!!';
+	}
 
-    public function setParkBrake(bool $hasParkBrake)
-{
-    $this->hasParkBrake = $hasParkBrake;
-}
+	public function setParkBrake()
+	{
+		$this->hasParkBrake = !$this->hasParkBrake;
+	}
 
-    public function start(): string
-{
-    if($this->hasParkBrake = true){
-        throw new Exception("You ride with the park brake!!!");
-    }
-    $this->currentSpeed = 10;
-    return "Start!";
-}
+	public function getParkBrake(): bool
+	{
+		return $this->hasParkBrake;
+	}
